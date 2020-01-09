ac8900fff358f7e28722753116d1ef1ee85a607b
Deivid <deivid.silveira@technobox.com.br>
2020-01-09 13:36:46 -0300

    new

--- flask/index.py
+++ flask/index.py
@@ -2,7 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class game:
+    def __init__(self, name, category, console):
+        self.name = name
+        self.category = category
+        self.console = console
+
+
+
+
 @app.route('/')
 def hello():
-    return render_template('lista.html', title = 'Games')
+    game1 = game('Super Mario', 'Acao', 'SNES')
+    game2 = game('Pokemon Gold', 'RPG', 'GBA')
+    listy = [game1, game2]
+    return render_template('lista.html', title = 'Games', game = listy)
 app.run()
